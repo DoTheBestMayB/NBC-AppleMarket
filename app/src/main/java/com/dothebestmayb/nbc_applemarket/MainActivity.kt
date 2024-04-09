412ed3869ec9514c5759d191fb90d49d1099eb17
DoDoBest <send1684@gmail.com>
2024-04-09 12:31:23 +0900

    feat: 상단 툴바 제거

--- app/src/main/java/com/dothebestmayb/nbc_applemarket/MainActivity.kt
+++ app/src/main/java/com/dothebestmayb/nbc_applemarket/MainActivity.kt
@@ -2,10 +2,25 @@ package com.dothebestmayb.nbc_applemarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dothebestmayb.nbc_applemarket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
