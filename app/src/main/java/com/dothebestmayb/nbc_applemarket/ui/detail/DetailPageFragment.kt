449788154b7c1a276a05cd521d7bd39f46de42fb
DoDoBest <send1684@gmail.com>
2024-04-11 21:49:10 +0900

    chore: 채팅하기 버튼을 누르면 지원하지 않는다는 문구 표시

--- app/src/main/java/com/dothebestmayb/nbc_applemarket/ui/detail/DetailPageFragment.kt
+++ app/src/main/java/com/dothebestmayb/nbc_applemarket/ui/detail/DetailPageFragment.kt
@@ -155,6 +155,10 @@ class DetailPageFragment : Fragment() {
         vGuidance.root.setOnClickListener {
             it.visibility = View.VISIBLE
         }
+
+        btnChat.setOnClickListener {
+            Toast.makeText(requireContext(), R.string.chat_is_not_supported, Toast.LENGTH_SHORT).show()
+        }
     }
 
 
